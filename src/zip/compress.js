3f2c28ea17681e04472590f64ada2642d9633df2
P0krovskii <dudov.dmitriy.02@gmail.com>
2024-05-16 02:43:30 +0300

    create file compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const filePath = 'G:\\NudeJs\\mitso-nodejs-basic\\src\\zip\\files\\fileToCompress.txt';
+    const readStream = fs.createReadStream(filePath);
+    const writeStream = fs.createWriteStream('archive.gz');
+    const gzip = zlib.createGzip();
+
+    readStream.pipe(gzip).pipe(writeStream);
+
+    await new Promise((resolve, reject) => {
+        writeStream.on('finish', resolve);
+        writeStream.on('error', reject);
+    });
+
+    await fs.promises.unlink(filePath);
 };
 
 await compress();

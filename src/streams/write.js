c1e38a756e0ee12ffd0afda785f3ee8eedb094a3
P0krovskii <dudov.dmitriy.02@gmail.com>
2024-05-16 02:37:31 +0300

    create file write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,36 @@
+import fs from 'fs';
+const filePath = 'G:\\NudeJs\\mitso-nodejs-basic\\src\\streams\\files\\fileToWrite.txt';
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = fs.createWriteStream(filePath);
+
+    writeStream.on('error', (err) => {
+        console.error('Ошибка записи:', err);
+        process.exit(1);
+    });
+
+    await new Promise((resolve) => {
+        writeStream.once('open', resolve);
+    });
+
+    process.stdin.on('data', (data) => {
+        if (data.toString().trim() === 'exit') {
+            console.log('Процесс записи завершается по запросу пользователя.');
+            writeStream.end();
+            process.exit(0);
+        }
+    });
+
+    process.stdin.pipe(writeStream);
+
+    await new Promise((resolve) => {
+        writeStream.on('finish', resolve);
+    });
+
+    console.log('Данные успешно записаны в файл', filePath);
+    writeStream.end();
+
+    process.exit(0);
 };
 
 await write();

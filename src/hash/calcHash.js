f86eb9c87b00074c96b867f65b9db9f302cd8f6d
P0krovskii <dudov.dmitriy.02@gmail.com>
2024-05-16 02:10:56 +0300

    create file Calulate_Hash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import fs from 'fs/promises';
+import path from 'path';
 
-await calculateHash();
+async function calcHash() {
+    const sourceFolder = 'G:\\NudeJs\\mitso-nodejs-basic\\src\\fs\\files';
+    const oldFilename = 'fileToCalculateHashFor.txt';
+
+    const sourceFilePath = path.join(sourceFolder, oldFilename);
+
+    try {
+        await fs.access(sourceFilePath);
+        console.error('Not file in folder');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            try {
+                const bufferText = Buffer.from(oldFilename, 'utf8');
+                console.log(bufferText.toString('hex'));
+            } catch {
+                
+            }
+        }
+    }
+
+}
+
+calcHash();
+

d65ec07d4891e208faab3d097271107c20a4e7cb
P0krovskii <dudov.dmitriy.02@gmail.com>
2024-05-16 02:05:33 +0300

    create file list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
+import fs from 'fs/promises';
+import path from 'path';
 const list = async () => {
-    // Write your code here 
+
+  const folderPath = 'G:\\NudeJs\\mitso-nodejs-basic\\src\\fs\\files';
+
+  try {
+    // Проверка папки
+    await fs.access(folderPath, fs.constants.F_OK);
+
+    // Поиск содержания
+    const fileNames = await fs.readdir(folderPath);
+
+    console.log('File names in "files" folder:', fileNames);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.error('FS operation failed: Folder does not exist');
+    } else {
+      console.error('FS operation failed:', error.message);
+    }
+  }
+
 };
 
 await list();

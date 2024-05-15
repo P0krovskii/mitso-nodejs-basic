5a494c5a36a0666d88c06a36d3f41107b5d94394
P0krovskii <dudov.dmitriy.02@gmail.com>
2024-05-16 02:03:54 +0300

    create file delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,26 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 const remove = async () => {
-    // Write your code here 
+const folderPath = 'G:\\NudeJs\\mitso-nodejs-basic\\src\\fs\\files';
+const filename = 'fileToRemove.txt';
+const filePath = path.join(folderPath, filename);
+
+try {
+  // Проверка
+  await fs.access(filePath, fs.constants.F_OK);
+
+  // Удаление 
+  await fs.unlink(filePath);
+  console.log('File deleted successfully');
+} catch (error) {
+  if (error.code === 'ENOENT') {
+    console.error('FS operation failed: File does not exist');
+  } else {
+    console.error('FS operation failed:', error.message);
+  }
+}
+
 };
 
 await remove();

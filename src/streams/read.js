e064fa2b106795699ef4889d018ab439c04b6faf
P0krovskii <dudov.dmitriy.02@gmail.com>
2024-05-16 02:31:00 +0300

    create file streams_read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,28 @@
-const read = async () => {
-    // Write your code here 
+import fs from 'fs';
+
+const readAndPrintFile = async () => {
+    const filePath = 'G:\\NudeJs\\mitso-nodejs-basic\\src\\streams\\files\\fileToRead.txt';
+    const fileStream = fs.createReadStream(filePath);
+
+    return new Promise((resolve, reject) => {
+        fileStream.on('data', (chunk) => {
+            process.stdout.write(chunk);
+        });
+
+        fileStream.on('end', () => {
+            resolve();
+        });
+
+        fileStream.on('error', (error) => {
+            reject(error);
+        });
+    });
 };
 
-await read();
+(async () => {
+    try {
+        await readAndPrintFile();
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+})();
